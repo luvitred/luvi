9150178b5ecbb4527475e441f2e288aaa67b3738
Timothy <t.vanwonterghem@option.com>
2018-10-11 16:17:39 +0200

    Revert including lpeg re "fix #167"
    This reverts commit f98590eeee8b9220d2c0219919cdb85723ecdfa4.

--- deps/lpeg.cmake
+++ deps/lpeg.cmake
@@ -1,16 +1,25 @@
-include_directories(
-  ${LPEGLIB_DIR}
-)
 
-add_library(LPEGLIB
-  ${LPEGLIB_DIR}/lpcap.c
-  ${LPEGLIB_DIR}/lpcode.c
-  ${LPEGLIB_DIR}/lpprint.c
-  ${LPEGLIB_DIR}/lptree.c
-  ${LPEGLIB_DIR}/lpvm.c
-)
+if (WithSharedLPEG)
+  find_package(LPEG REQUIRED)
+  link_directories(${LPEGLIB})
+  set(EXTRA_LIBS ${EXTRA_LIBS} ${LPEGLIB})
+else (WithSharedPCRE)
+  set(LPEGLIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/lpeg)
 
-set(EXTRA_LIBS ${EXTRA_LIBS} LPEGLIB)
+  include_directories(
+    ${LPEGLIB_DIR}
+  )
+
+  add_library(LPEGLIB
+    ${LPEGLIB_DIR}/lpcap.c
+    ${LPEGLIB_DIR}/lpcode.c
+    ${LPEGLIB_DIR}/lpprint.c
+    ${LPEGLIB_DIR}/lptree.c
+    ${LPEGLIB_DIR}/lpvm.c
+  )
+
+  set(EXTRA_LIBS ${EXTRA_LIBS} LPEGLIB)
+endif (WithSharedLPEG)
 
 add_definitions(-DLUA_LIB -DWITH_LPEG)
 remove_definitions(-DNDEBUG)
