604d090ddeb18d63212b1a65b54ff291e2e3ffe9
Timothy <t.vanwonterghem@option.com>
2023-04-17 14:38:46 +0200

    snapshot update
    update from upstream: https://github.com/cloudwu/lua-snapshot
    
    luajit now pushes a cfunction when luaL_newstate is used.
    pdesc now checks if the type is a table when iterating.

--- src/snapshot.c
+++ src/snapshot.c
@@ -68,9 +68,23 @@ mark_function_env(lua_State *L, lua_State *dL, const void * t) {
 	}
 }
 
+// lua 5.1 has no light c function
+#define is_lightcfunction(L, idx) (0)
+
 #else
 
 #define mark_function_env(L,dL,t)
+
+static int
+is_lightcfunction(lua_State *L, int idx) {
+	if (lua_iscfunction(L, idx)) {
+		if (lua_getupvalue(L, idx, 1) == NULL) {
+			return 1;
+		}
+		lua_pop(L, 1);
+	}
+	return 0;
+}
 
 #endif
 
@@ -107,6 +121,10 @@ readobject(lua_State *L, lua_State *dL, const void *parent, const char *desc) {
 		tidx = TABLE;
 		break;
 	case LUA_TFUNCTION:
+		if (is_lightcfunction(L, -1)) {
+			lua_pop(L, 1);
+			return NULL;
+		}
 		tidx = FUNCTION;
 		break;
 	case LUA_TTHREAD:
@@ -116,6 +134,7 @@ readobject(lua_State *L, lua_State *dL, const void *parent, const char *desc) {
 		tidx = USERDATA;
 		break;
 	default:
+		lua_pop(L, 1);
 		return NULL;
 	}
 
@@ -140,22 +159,22 @@ readobject(lua_State *L, lua_State *dL, const void *parent, const char *desc) {
 }
 
 static const char *
-keystring(lua_State *L, int index, char * buffer) {
+keystring(lua_State *L, int index, char * buffer, size_t size) {
 	int t = lua_type(L,index);
 	switch (t) {
 	case LUA_TSTRING:
 		return lua_tostring(L,index);
 	case LUA_TNUMBER:
-		sprintf(buffer,"[%lg]",lua_tonumber(L,index));
+		snprintf(buffer, size, "[%lg]",lua_tonumber(L,index));
 		break;
 	case LUA_TBOOLEAN:
-		sprintf(buffer,"[%s]",lua_toboolean(L,index) ? "true" : "false");
+		snprintf(buffer, size, "[%s]",lua_toboolean(L,index) ? "true" : "false");
 		break;
 	case LUA_TNIL:
-		sprintf(buffer,"[nil]");
+		snprintf(buffer, size, "[nil]");
 		break;
 	default:
-		sprintf(buffer,"[%s:%p]",lua_typename(L,t),lua_topointer(L,index));
+		snprintf(buffer, size, "[%s:%p]",lua_typename(L,t),lua_topointer(L,index));
 		break;
 	}
 	return buffer;
@@ -193,7 +212,7 @@ mark_table(lua_State *L, lua_State *dL, const void * parent, const char * desc) 
 			lua_pop(L,1);
 		} else {
 			char temp[32];
-			const char * desc = keystring(L, -2, temp);
+			const char * desc = keystring(L, -2, temp, sizeof(temp));
 			mark_object(L, dL, t , desc);
 		}
 		if (!weakk) {
@@ -218,7 +237,7 @@ mark_userdata(lua_State *L, lua_State *dL, const void * parent, const char *desc
 	if (lua_isnil(L,-1)) {
 		lua_pop(L,2);
 	} else {
-		mark_table(L, dL, t, "[uservalue]");
+		mark_object(L, dL, t, "[uservalue]");
 		lua_pop(L,1);
 	}
 }
@@ -238,17 +257,13 @@ mark_function(lua_State *L, lua_State *dL, const void * parent, const char *desc
 		mark_object(L, dL, t, name[0] ? name : "[upvalue]");
 	}
 	if (lua_iscfunction(L,-1)) {
-		if (i==1) {
-			// light c function
-			lua_pushnil(dL);
-			lua_rawsetp(dL, FUNCTION, t);
-		}
 		lua_pop(L,1);
 	} else {
 		lua_Debug ar;
 		lua_getinfo(L, ">S", &ar);
 		luaL_Buffer b;
 		luaL_buffinit(dL, &b);
+		luaL_addstring(&b, "func: ");
 		luaL_addstring(&b, ar.short_src);
 		char tmp[16];
 		sprintf(tmp,":%d",ar.linedefined);
@@ -267,6 +282,17 @@ mark_thread(lua_State *L, lua_State *dL, const void * parent, const char *desc) 
 	lua_State *cL = lua_tothread(L,-1);
 	if (cL == L) {
 		level = 1;
+	} else {
+		// mark stack
+		int top = lua_gettop(cL);
+		luaL_checkstack(cL, 1, NULL);
+		int i;
+		char tmp[16];
+		for (i=0;i<top;i++) {
+			lua_pushvalue(cL, i+1);
+			sprintf(tmp, "[%d]", i+1);
+			mark_object(cL, dL, cL, tmp);
+		}
 	}
 	lua_Debug ar;
 	luaL_Buffer b;
@@ -294,6 +320,7 @@ mark_thread(lua_State *L, lua_State *dL, const void * parent, const char *desc) 
 
 		++level;
 	}
+	luaL_addstring(&b, "thread: ");
 	luaL_pushresult(&b);
 	lua_rawsetp(dL, SOURCE, t);
 	lua_pop(L,1);
@@ -336,7 +363,7 @@ count_table(lua_State *L, int idx) {
 static void
 gen_table_desc(lua_State *dL, luaL_Buffer *b, const void * parent, const char *desc) {
 	char tmp[32];
-	size_t l = sprintf(tmp,"%p : ",parent);
+	size_t l = snprintf(tmp, sizeof(tmp), "%p : ", parent);
 	luaL_addlstring(b, tmp, l);
 	luaL_addstring(b, desc);
 	luaL_addchar(b, '\n');
@@ -346,6 +373,10 @@ static void
 pdesc(lua_State *L, lua_State *dL, int idx, const char * typename) {
 	lua_pushnil(dL);
 	while (lua_next(dL, idx) != 0) {
+		if (lua_type(dL, -1) != LUA_TTABLE) {
+			lua_pop(dL,1);
+			continue;
+		}
 		luaL_Buffer b;
 		luaL_buffinit(L, &b);
 		const void * key = lua_touserdata(dL, -2);
@@ -360,14 +391,16 @@ pdesc(lua_State *L, lua_State *dL, int idx, const char * typename) {
 				luaL_addchar(&b,'\n');
 			}
 			lua_pop(dL, 1);
-		} else if (idx == THREAD) {
+		}
+		else if (idx == THREAD) {
 			lua_rawgetp(dL, SOURCE, key);
 			size_t l = 0;
 			const char * s = lua_tolstring(dL, -1, &l);
 			luaL_addlstring(&b,s,l);
 			luaL_addchar(&b,'\n');
 			lua_pop(dL, 1);
-		} else {
+		}
+		else {
 			luaL_addstring(&b, typename);
 			luaL_addchar(&b,'\n');
 		}
